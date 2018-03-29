fbe8626d78701a31b62c71371816d3510916a88c
Guillaume Massé <masgui@gmail.com>
2018-03-29 11:55:26 +0200

    Unit test for summary

--- tests/unit/src/test/scala/ch.epfl.scala.accessible/SummaryTests.scala
+++ tests/unit/src/test/scala/ch.epfl.scala.accessible/SummaryTests.scala
@@ -1,10 +1,35 @@
 package ch.epfl.scala.accessible
 
 import utest._
+import scala.meta._
 
 object SummaryTests extends TestSuite {
 
   val tests = Tests {
-    'summary - {}
+    'summary - {
+      check(
+        """|package org.example
+           |
+           |import foo._
+           |
+           |package object A { def f = 1 }
+           |object B { def f = 1 }
+           |class C { def f = 1 }
+           |trait D { def f = 1 }
+           |
+           |""".stripMargin,
+        """|package org example.
+           |package object A,
+           |object B,
+           |class C,
+           |trait D.""".stripMargin
+      )
+    }
+  }
+
+  def check(source: String, expected: String): Unit = {
+    val tree = source.parse[Source].get
+    val obtained = Summary(tree)
+    assert(obtained == expected)
   }
 }
