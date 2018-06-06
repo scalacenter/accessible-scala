0f73e63b3304d1148451609406b4effb5d6e8cc5
Guillaume Massé <masgui@gmail.com>
2018-06-06 14:14:47 +0200

    describe skeleton

--- lib/src/main/scala/ch.epfl.scala.accessible/Describe.scala
+++ lib/src/main/scala/ch.epfl.scala.accessible/Describe.scala
@@ -21,187 +21,161 @@ object Describe {
 
   def apply(tree: Tree): String = describe(tree)
 
-  def describe(tree: Tree): String =
-    tree match {
-      // Ctor.Primary(mods: List[Mod], name: Name, paramss: List[List[Term.Param]]) =>
-      case Ctor.Primary(mods, _, paramss) =>
-        ""
-
-      case Defn.Class(mods, name, tparams, ctor, templ) => {
-        val tparamsRes =
-          if (tparams.nonEmpty) tparams.map(describe).mkString("parametrized with: ", ",", "")
-          else ""
-
-        s"${join(mods)} class ${describe(name)} $tparamsRes ${describe(ctor)} ${describe(templ)}"
-      }
-
-      case Defn.Def(mods, name, tparams, paramss, decltpe, body) =>
-        dDef(mods, name, tparams, paramss, decltpe, Some(body))
-
-      case Decl.Def(mods, name, tparams, paramss, decltpe) =>
-        dDef(mods, name, tparams, paramss, Some(decltpe), None)
-
-      case Mod.Covariant()     => "co-variant"
-      case Mod.Contravariant() => "contra-variant"
-
-      // Template(early: List[Stat], inits: List[Init], self: Self, stats: List[Stat])
-      case Template(early, inits, self, stats) =>
-        ""
-      case Term.Name(value) =>
-        value
-      case Term.Param(mods, name, decltpe, default) =>
-        List(
-          join(mods),
-          describe(name),
-          decltpe.map(describe).getOrElse(""),
-          default.map(describe).getOrElse("")
-        ).filter(_.nonEmpty).mkString(" ")
-
-      case Type.Annotate(tpe, annots) =>
-        s"${describe(tpe)} annotated with: ${join(annots)}"
-
-      case Type.Apply(tpt, args) =>
-        val allPlaceholders = args.forall {
-          case Type.Placeholder(Type.Bounds(None, None)) => true
-          case _                                         => false
-        }
-
-        if (allPlaceholders) s"${describe(tpt)} taking ${args.size} parameters"
-        else {
-          val verb =
-            if (args.size == 1) "of"
-            else "applied to"
-
-          List(
-            describe(tpt),
-            verb,
-            join(args)
-          ).filter(_.nonEmpty).mkString(" ")
-        }
-
-      case Type.ApplyInfix(lhs, op, rhs) =>
-        s"${describe(lhs)} ${describe(op)} ${describe(rhs)}"
-
-      case Type.Bounds(lower, higher) => {
-        List(
-          lower.map(l => "super-type of: " + describe(l)).getOrElse(""),
-          higher.map(h => "sub-type of: " + describe(h)).getOrElse("")
-        ).filter(_.nonEmpty).mkString(", ")
-      }
-
-      case Type.ByName(tpe) =>
-        "by name: " + describe(tpe)
-
-      case Type.Name(value) =>
-        value
-
-      case Type.Function(params, res) => {
-        val dParams =
-          if (params.nonEmpty) join(params)
-          else "Unit"
-
-        "function " + dParams + " to " + describe(res)
-      }
-      case Type.Param(mods, name, tparams, tbounds, vbounds, cbounds) => {
-
-        val vboundsRes =
-          if (vbounds.nonEmpty) vbounds.map(describe).mkString("view bounded by: ", ", ", ".")
-          else ""
-
-        val cboundsRes =
-          if (cbounds.nonEmpty) cbounds.map(describe).mkString("context bounded by: ", ", ", ".")
-          else ""
-
-        val tparamsRes =
-          if (tparams.nonEmpty) tparams.map(describe).mkString("of", ", ", "")
-          else ""
-
-        val nameRes =
-          if (name.is[Name.Anonymous]) "a parameter"
-          else describe(name)
-
-        List(
-          join(mods),
-          nameRes,
-          tparamsRes,
-          describe(tbounds),
-          vboundsRes,
-          cboundsRes
-        ).filter(_.nonEmpty).mkString(" ")
-      }
-      case Type.Project(qual, name) =>
-        s"${describe(qual)} project ${describe(name)}"
-
-      // class Type.Placeholder(bounds: Bounds)
-
-      case Type.Select(qual, name) =>
-        describe(qual) + " dot " + describe(name)
-      case Type.Singleton(ref) =>
-        s"singleton ${describe(ref)}"
-      case Type.Tuple(args) =>
-        s"tuple ${args.size} of " + join(args)
-
-      case Type.Refine(tpe, stats) =>
-        List(
-          "type refinement: ",
-          tpe.map(describe).getOrElse(""),
-          join(stats)
-        ).filter(_.nonEmpty).mkString(" ")
-
-      case Type.Repeated(tpe) =>
-        "repeated: " + describe(tpe)
-
-      // class Var(name: Name)
-
-      case Type.With(lhs, rhs) =>
-        s"${describe(lhs)} with ${describe(rhs)}"
-
-      // Dotty
-      // class Lambda(tparams: List[Type.Param], tpe: Type)
-      // class ImplicitFunction(params: List[Type], res: Type)
-      // class And(lhs: Type, rhs: Type)
-      // class Or(lhs: Type, rhs: Type)
-
-      // // Synthetic
-      // class Method(paramss: List[List[Term.Param]], tpe: Type)
-
-      // // Todo
-      // class Existential(tpe: Type, stats: List[Stat])
-
-      // case e => e.syntax
-      case _ => ""
-    }
-
-  private def dDef(mods: List[Mod],
-                   name: Term.Name,
-                   tparams: List[scala.meta.Type.Param],
-                   paramss: List[List[Term.Param]],
-                   decltpe: Option[scala.meta.Type],
-                   body: Option[Term]): String = {
-
-    val tparamsRes =
-      if (tparams.nonEmpty) tparams.map(describe).mkString("parametrized with: ", ", ", "")
-      else ""
-
-    val paramssRes =
-      if (paramss.nonEmpty) paramss.flatMap(_.map(describe)).mkString(", ")
-      else ""
-
-    List(
-      join(mods),
-      "def",
-      describe(name),
-      tparamsRes,
-      paramssRes
-    ).filter(_.nonEmpty).mkString(" ") +
-      List(
-        decltpe.map(tpe => ".\nreturns: " + describe(tpe)).getOrElse(""),
-        body.map(b => ".\nbody: " + describe(b)).getOrElse(""),
-      ).filter(_.nonEmpty).mkString(" ")
+  def describe(tree: Tree): String = tree match {
+    case lit: Lit                              => describe(lit)
+    case term: Term                            => describe(term)
+    case tpe: Type                             => describe(tpe)
+    case pat: Pat                              => describe(pat)
+    case decl: Decl                            => describe(decl)
+    case defn: Defn                            => describe(defn)
+    case mod: Mod                              => describe(mod)
+    case enum: Enumerator                      => describe(enum)
+    case ctor: Ctor                            => describe(ctor)
+    case _: Import | _: Importer | _: Importee => describeImports(tree)
+    case _ => describeMisc(tree)
   }
 
-  private def join(args: List[Tree]): String =
-    args.map(describe).mkString(", ")
+  def describe(lit: Lit): String = lit.syntax
 
+  def describe(term: Term): String = term match {
+    case This(qual)                             => "todo"
+    case Super(thisp, superp)                   => "todo"
+    case Name(value)                            => "todo"
+    case Select(qual, name)                     => "todo"
+    case Interpolate(prefix, parts, args)       => "todo"
+    case Xml(parts, args)                       => "todo"
+    case Apply(fun, args)                       => "todo"
+    case ApplyType(fun, targs)                  => "todo"
+    case ApplyInfix(lhs, op, targs, args)       => "todo"
+    case Assign(lhs, rhs)                       => "todo"
+    case Return(expr)                           => "todo"
+    case Throw(expr)                            => "todo"
+    case Ascribe(expr, tpe)                     => "todo"
+    case Annotate(expr, annots)                 => "todo"
+    case Tuple(args)                            => "todo"
+    case Block(stats)                           => "todo"
+    case If(cond, thenp, elsep)                 => "todo"
+    case Match(expr, cases)                     => "todo"
+    case Try(expr, catchp, finallyp)            => "todo"
+    case TryWithHandler(expr, catchp, finallyp) => "todo"
+    case Function(params, body)                 => "todo"
+    case PartialFunction(cases)                 => "todo"
+    case While(expr, body)                      => "todo"
+    case Do(body, expr)                         => "todo"
+    case For(enums, body)                       => "todo"
+    case ForYield(enums, body)                  => "todo"
+    case New(init)                              => "todo"
+    case NewAnonymous(templ)                    => "todo"
+    case Placeholder()                          => "todo"
+    case Eta(expr)                              => "todo"
+    case Repeated(expr)                         => "todo"
+    case Param(mods, name, decltpe, default)    => "todo"
+  }
+
+  def describe(tpe: Type): String = tpe match {
+    case Name(value)                                           => "todo"
+    case Select(qual, name)                                    => "todo"
+    case Project(qual, name)                                   => "todo"
+    case Singleton(ref)                                        => "todo"
+    case Apply(tpe, args)                                      => "todo"
+    case ApplyInfix(lhs, op, rhs)                              => "todo"
+    case Function(params, res)                                 => "todo"
+    case ImplicitFunction(params, res)                         => "todo"
+    case Tuple(args)                                           => "todo"
+    case With(lhs, rhs)                                        => "todo"
+    case And(lhs, rhs)                                         => "todo"
+    case Or(lhs, rhs)                                          => "todo"
+    case Refine(tpe, stats)                                    => "todo"
+    case Existential(tpe, stats)                               => "todo"
+    case Annotate(tpe, annots)                                 => "todo"
+    case Lambda(tparams, tpe)                                  => "todo"
+    case Method(paramss, tpe)                                  => "todo"
+    case Placeholder(bounds)                                   => "todo"
+    case Bounds(lo, hi)                                        => "todo"
+    case ByName(tpe)                                           => "todo"
+    case Repeated(tpe)                                         => "todo"
+    case Var(name)                                             => "todo"
+    case Param(mods, name, tparams, tbounds, vbounds, cbounds) => "todo"
+  }
+
+  def describe(pat: Pat): String = pat match {
+    case Var(name)                        => "todo"
+    case Wildcard()                       => "todo"
+    case SeqWildcard()                    => "todo"
+    case Bind(lhs, rhs)                   => "todo"
+    case Alternative(lhs, rhs)            => "todo"
+    case Tuple(args)                      => "todo"
+    case Extract(fun, args)               => "todo"
+    case ExtractInfix(lhs, op, rhs)       => "todo"
+    case Interpolate(prefix, parts, args) => "todo"
+    case Xml(parts, args)                 => "todo"
+    case Typed(lhs, rhs)                  => "todo"
+    case _                                => "todo"
+  }
+
+  def describe(decl: Decl): String = decl match {
+    case Val(mods, pats, decltpe)                   => "todo"
+    case Var(mods, pats, decltpe)                   => "todo"
+    case Def(mods, name, tparams, paramss, decltpe) => "todo"
+    case Type(mods, name, tparams, bounds)          => "todo"
+  }
+
+  def describe(defn: Defn): String = defn match {
+    case Val(mods, pats, decltpe, rhs)                      => "todo"
+    case Var(mods, pats, decltpe, rhs)                      => "todo"
+    case Def(mods, name, tparams, paramss, decltpe, body)   => "todo"
+    case Macro(mods, name, tparams, paramss, decltpe, body) => "todo"
+    case Type(mods, name, tparams, body)                    => "todo"
+    case Class(mods, name, tparams, ctor, templ)            => "todo"
+    case Trait(mods, name, tparams, ctor, templ)            => "todo"
+    case Object(mods, name, templ)                          => "todo"
+  }
+
+  def describe(mod: Mod): String = {
+    case Annot(init)       => "todo"
+    case Private(within)   => "todo"
+    case Protected(within) => "todo"
+    case Covariant()       => "todo"
+    case Contravariant()   => "todo"
+    case Implicit()        => "todo"
+    case Final()           => "todo"
+    case Sealed()          => "todo"
+    case Override()        => "todo"
+    case Case()            => "todo"
+    case Abstract()        => "todo"
+    case Lazy()            => "todo"
+    case ValParam()        => "todo"
+    case VarParam()        => "todo"
+    case Inline()          => "todo"
+  }
+
+  def describe(enum: Enumerator): String = enum match {
+    case Generator(pat, rhs) => "todo"
+    case Val(pat, rhs)       => "todo"
+    case Guard(cond)         => "todo"
+  }
+
+  def describe(ctor: Ctor): String = ctor match {
+    case Primary(mods, name, paramss)                => "todo"
+    case Secondary(mods, name, paramss, init, stats) => "todo"
+  }
+
+  def describeImports(tree: Tree): String = tree match {
+    case Import(importers)        => "todo"
+    case Importer(ref, importees) => "todo"
+    case Wildcard()               => "todo"
+    case Name(name)               => "todo"
+    case Rename(name, rename)     => "todo"
+    case Unimport(name)           => "todo"
+  }
+
+  def describeMisc(tree: Tree): String = tree match {
+    case Source(stats)                       => "todo"
+    case Case(pat, cond, body)               => "todo"
+    case Pkg(ref, stats)                     => "todo"
+    case Pkg.Object(mods, name, templ)       => "todo"
+    case Init(tpe, name, argss)              => "todo"
+    case Self(name, decltpe)                 => "todo"
+    case Template(early, inits, self, stats) => "todo"
+  }
 }
-// class TreeMap, parametrized with: A view bound to Comparable of A, B
