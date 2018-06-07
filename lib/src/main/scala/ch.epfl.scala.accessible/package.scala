463ed6548c91ffd9ba4ba4f78660fe8e1261856f
Guillaume Massé <masgui@gmail.com>
2018-06-07 16:11:44 +0200

    generalize pretty(Tree) for pretty(List[Tree])

--- lib/src/main/scala/ch.epfl.scala.accessible/package.scala
+++ lib/src/main/scala/ch.epfl.scala.accessible/package.scala
@@ -42,66 +42,73 @@ package object accessible {
     found
   }
 
-  def pretty(tree: Tree, showFields: Boolean = false): String = {
-    def wrapList(args: List[Doc]): Doc = {
-      wrap(Doc.text("List") + Doc.char('('), args, Doc.char(')'))
-    }
+  def wrapList(args: List[Doc]): Doc = {
+    if (args.nonEmpty) wrap(Doc.text("List") + Doc.char('('), args, Doc.char(')'))
+    else Doc.text("Nil")
+  }
 
-    def wrap(prefix: Doc, args: List[Doc], suffix: Doc): Doc = {
-      val body = Doc.intercalate(Doc.char(',') + Doc.line, args)
-      body.tightBracketBy(prefix, suffix)
-    }
+  def wrap(prefix: Doc, args: List[Doc], suffix: Doc): Doc = {
+    val body = Doc.intercalate(Doc.char(',') + Doc.line, args)
+    body.tightBracketBy(prefix, suffix)
+  }
 
-    def prettyList(vs: List[Tree]): Doc =
-      wrapList(vs.map(v => prettyFromProduct(v)))
+  def prettyDoc(tree: List[Tree], showFields: Boolean): Doc = {
+    wrapList(tree.map(t => prettyDoc(t, showFields)))
+  }
 
-    def prettyFromProduct(tree0: Tree): Doc = {
-      tree0 match {
-        case _ if tree0.tokens.isEmpty => Doc.empty
-        case v: Term.Name              => Doc.text(v.structure)
-        case t: Type.Name              => Doc.text(t.structure)
-        case _ => {
-          val args =
-            tree0.productFields.zip(tree0.productIterator.toList).map {
-              case (k, v) =>
-                val rhs =
-                  v match {
-                    case v: Term.Name => Doc.text(v.structure)
-                    case t: Tree      => prettyFromProduct(t)
-                    case o: Option[_] =>
-                      o match {
-                        case Some(t: Tree) =>
-                          wrap(
-                            Doc.text("Some") + Doc.char('('),
-                            List(prettyFromProduct(t)),
-                            Doc.char(')')
-                          )
-                        case None => Doc.text("None")
-                        case _    => throw new Exception("cannot handle: " + o)
+  def prettyDoc(tree: Tree, showFields: Boolean): Doc = {
+    tree match {
+      case _ if tree.tokens.isEmpty => Doc.empty
+      case v: Term.Name             => Doc.text(v.structure)
+      case t: Type.Name             => Doc.text(t.structure)
+      case _ => {
+        val args =
+          tree.productFields.zip(tree.productIterator.toList).map {
+            case (k, v) =>
+              val rhs =
+                v match {
+                  case v: Term.Name => Doc.text(v.structure)
+                  case t: Tree      => prettyDoc(t, showFields)
+                  case o: Option[_] =>
+                    o match {
+                      case Some(t: Tree) =>
+                        wrap(
+                          Doc.text("Some") + Doc.char('('),
+                          List(prettyDoc(t, showFields)),
+                          Doc.char(')')
+                        )
+                      case None => Doc.text("None")
+                      case _    => throw new Exception("cannot handle: " + o)
+                    }
+                  case vs: List[_] =>
+                    vs match {
+                      case Nil => Doc.text("Nil")
+                      case (h: Tree) :: _ => {
+                        prettyDoc(vs.asInstanceOf[List[Tree]], showFields)
                       }
-                    case vs: List[_] =>
-                      vs match {
-                        case Nil => Doc.text("Nil")
-                        case (h: Tree) :: _ => {
-                          prettyList(vs.asInstanceOf[List[Tree]])
-                        }
-                        case (h: List[_]) :: _ => {
-                          val vsT = vs.asInstanceOf[List[List[Tree]]]
-                          wrapList(vsT.map(prettyList))
-                        }
-                        case _ => throw new Exception("cannot handle: " + vs)
+                      case (h: List[_]) :: _ => {
+                        val vsT = vs.asInstanceOf[List[List[Tree]]]
+                        wrapList(vsT.map(v => prettyDoc(v, showFields)))
                       }
-                    case _ => Doc.text(v.toString)
-                  }
+                      case _ => throw new Exception("cannot handle: " + vs)
+                    }
+                  case _ => Doc.text(v.toString)
+                }
 
-                if (showFields) Doc.text(k) + Doc.text(" = ") + rhs
-                else rhs
-            }
+              if (showFields) Doc.text(k) + Doc.text(" = ") + rhs
+              else rhs
+          }
 
-          wrap(Doc.text(tree0.productPrefix) + Doc.char('('), args, Doc.char(')'))
-        }
+        wrap(Doc.text(tree.productPrefix) + Doc.char('('), args, Doc.char(')'))
       }
     }
-    prettyFromProduct(tree).render(1)
+  }
+
+  def pretty(tree: Tree, showFields: Boolean = false): String = {
+    prettyDoc(tree, showFields).render(1)
+  }
+
+  def pretty(tree: List[Tree], showFields: Boolean): String = {
+    prettyDoc(tree, showFields).render(1) 
   }
 }
